8b30851636f951b573b9d23bf68935dd0aeb6b7d
Pete Emerson <pete@theemersons.org>
2014-08-15 10:46:52 -0700

    Modifications to the client to make it work properly with a wrapper

--- foo_client.py
+++ foo_client.py
@@ -1,42 +1,33 @@
-import requests
-import json
-import inspect
-import sys
+#!/usr/bin/env python
+
 from requests import Request, Session
-import pprint
+import json
 
-schema = { "thing" : { "get" : ['foo'] } }
-
-class Foo_Client(object):
+class Foo_Client:
 
   def __init__(self, hostname='localhost', port=5000):
     self.hostname = hostname
     self.port = port
     self.url = 'http://' + self.hostname + ':' + str(self.port)
-    for name in schema:
-      for method in schema[name]:
-        function_name = method + '_' + name
-        setattr(self, function_name, self.func)
+    # This really should come from a file. Perhaps an auto-generated one.
+    self.data = { 'server' : ['get'], 'user' : ['get']}
 
-  def func(self, **kwargs):
-    for k, v in vars(self).items():
-      if inspect.ismethod(v):
-        function_name = k
-        break
+    for endpoint in self.data:
+      for method in self.data[endpoint]:
+        function_name = method + '_' + endpoint
+        method = method.upper()
+        setattr(self, function_name, self.wrap(method, endpoint))
 
-    method, path = function_name.split('_')
-    method = method.upper()
-    url = self.url + '/' + path
-    data = kwargs
-
-    header = {'content-type':'application/json'}
-
-    s = Session()
-    req = Request(method, url,
-        data=json.dumps(kwargs),
-        headers=header
-    )
-    prepped = req.prepare()
-
-    r = s.send(prepped)
-    return r.text
+  def wrap(self, method, endpoint):
+    def  catch_all(**kwargs):
+      url = self.url + '/' + endpoint
+      header = {'content-type':'application/json'}
+      s = Session()
+      req = Request(method, url,
+          data=json.dumps(kwargs),
+          headers=header
+      )
+      prepped = req.prepare()
+      r = s.send(prepped)
+      return r.text.rstrip()
+    return catch_all
