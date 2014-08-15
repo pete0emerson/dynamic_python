0fcd2abe4d44456b4d6d8140f52bde157967e339
Pete Emerson <pete@theemersons.org>
2014-08-15 14:17:17 -0700

    Function should handle optional arguments

--- foo_library.py
+++ foo_library.py
@@ -1,13 +1,13 @@
 import json
 
-def get_server(environment=None):
+def get_server(environment=None, **kwargs):
   if environment:
     # This might in reality make a database call, of course
     data = {"status":"okay", "environment": str(environment)}
     return data
   return None
 
-def get_user(name=None):
+def get_user(name=None, **kwargs):
   if name:
     data = {"status":"okay", "name": str(name)}
     return data
